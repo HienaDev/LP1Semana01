26a87df232215cd826ccea002d60d2cd33f1b743
HienaDev <ant.rodrigues1999@gmail.com>
2023-03-01 17:51:44 +0000

    Added comments to the programs

--- Argumentos/Program.cs
+++ Argumentos/Program.cs
@@ -1,11 +1,17 @@
 ﻿using System;
 
+// Namespace's name, useful when you'd like to use this program on another 
+// program with the using tag, like in "using System"
 namespace Argumentos
 {
+    // Name of the class
     class Program
     {
+        // Name of the function and arguments it receives, main receives
+        // arguments from the cmd line
         static void Main(string[] args)
         {
+            //Prints all the arguments received from the cmd line
             for (int i = 0; i < args.Length ; i++)
             {
                 Console.WriteLine(args[i]);
--- OlaMundo/Program.cs
+++ OlaMundo/Program.cs
@@ -1,11 +1,17 @@
 ﻿using System;
 
+// Namespace's name, useful when you'd like to use this program on another 
+// program with the using tag, like in "using System"
 namespace OlaMundo
 {
+    // Name of the class
     class Program
     {
+        // Name of the function and arguments it receives, main receives
+        // arguments from the cmd line
         static void Main(string[] args)
         {
+            //Prints these phrases to the terminal
             Console.WriteLine("Hello, World!");
             Console.WriteLine("Bye world");
         }
