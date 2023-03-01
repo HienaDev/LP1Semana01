2834e77d67a1ab6b24d12560eb8d1d9dfe0a3b5b
HienaDev <ant.rodrigues1999@gmail.com>
2023-03-01 18:14:50 +0000

    Added ints

---
+++ TiposVarios/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace TiposVarios
+{
+    class Program
+    {
+
+        
+
+
+        static void Main(string[] args)
+        {
+            int numero1 = 10;
+            int numero2 = 0x89;
+            int numero3 = 0b10101001;
+            int numero4 = 0;
+            int numero5 = -123;
+
+            Console.WriteLine(numero1);
+            Console.WriteLine(numero2);
+            Console.WriteLine(numero3);
+            Console.WriteLine(numero4);
+            Console.WriteLine(numero5);
+        }
+    }
+}
