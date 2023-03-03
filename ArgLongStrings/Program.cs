657d69f26fcc3d241c076399b9831dfb33689d9b
HienaDev <ant.rodrigues1999@gmail.com>
2023-03-03 13:18:06 +0000

    Added ArgLongStrings

---
+++ ArgLongStrings/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+// Namespace's name, useful when you'd like to use this program on another 
+// program with the using tag, like in "using System"
+namespace ArgLongStrings
+{
+    // Name of the class
+    class Program
+    {
+        // Name of the function and arguments it receives, main receives
+        // arguments from the cmd line
+        static void Main(string[] args)
+        {
+            for (int i = 0; i < args.Length; i++)
+            {
+                if (args[i].Length > 4)
+                {
+                    Console.WriteLine(args[i].ToUpper());
+                }
+            }
+        }
+    }
+}
